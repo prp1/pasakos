c0ffab74074edc114d01d08690978028ed4da40e
Paulius R <paulius.repsys@visma.com>
2016-09-21 00:04:55 +0300

    Made story view to work

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,16 +1,18 @@
 import { HomeComponent } from "./pages/home/home.component";
 import { CategoriesComponent } from "./pages/categories/categories.component";
 import { StoriesListComponent } from "./pages/stories-list/stories-list.component";
+import { StoryViewComponent } from "./pages/story-view/story-view.component";
 
 export const routes = [
-  { path: "", component: HomeComponent },
-  { path: "categories", component: CategoriesComponent },
-  { path: "storieslist/:categoryid", component: StoriesListComponent },
-  { path: "stories/:id", component: StoriesListComponent },
+    { path: "", component: HomeComponent },
+    { path: "categories", component: CategoriesComponent },
+    { path: "storieslist/:categoryid", component: StoriesListComponent },
+    { path: "stories/:id", component: StoryViewComponent },
 ];
 
 export const navigatableComponents = [
-  HomeComponent,
-  CategoriesComponent,
-  StoriesListComponent,
+    HomeComponent,
+    CategoriesComponent,
+    StoriesListComponent,
+    StoryViewComponent,
 ];
