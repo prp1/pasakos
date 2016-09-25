e4ab9a4b8c92f694774a71958b51cc454ddd5411
Paulius R <paulius.repsys@visma.com>
2016-09-25 14:31:00 +0300

    Initial audio implementation

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,7 @@
 import {Component} from "@angular/core";
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { action } from 'ui/dialogs';
+import { TNSPlayer } from 'nativescript-audio';
 
 @Component({
     selector: "my-app",
@@ -7,6 +9,8 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 })
 export class AppComponent {
     public counter: number = 16;
+    private _player: TNSPlayer;
+    private _isPlaying = false;
 
     constructor(
         private route: ActivatedRoute,
@@ -16,5 +20,40 @@ export class AppComponent {
     public onActionBarClick() {
         this.router.navigate(['/']);
     }
-    
+
+    public onPlayMusicClick() {
+
+        if (!this._player) {
+            this._player = new TNSPlayer();
+        }
+
+        // var options = {
+        //     title: "Race Selection",
+        //     message: "Choose your race",
+        //     cancelButtonText: "Cancel",
+        //     actions: ["Human", "Elf", "Dwarf", "Orc"]
+        // };
+
+        // action(options).then((result) => {
+        //     console.log(result);
+        // });
+
+        if (!this._isPlaying) {
+            this._player.playFromFile({
+                audioFile: '~/audio/song1.mp3',
+                completeCallback: () => {
+
+                },
+                errorCallback: () => {
+
+                }
+            })
+
+            this._isPlaying = true;
+        } else {
+            this._player.pause();
+            this._isPlaying = false;
+        }
+
+    }
 }
