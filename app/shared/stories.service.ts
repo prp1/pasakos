782f27217ae0cdade01618e45d82d04010d7b9db
Paulius R <paulius.repsys@visma.com>
2016-09-29 20:02:15 +0300

    Initial bottom bar imiplementation

--- app/shared/stories.service.ts
+++ app/shared/stories.service.ts
@@ -19,7 +19,43 @@ export class StoriesService {
         {
             id: 3,
             title: 'Linksmos',
-        },        
+        },
+        {
+            id: 3,
+            title: 'Linksmos',
+        },
+        {
+            id: 3,
+            title: 'Linksmos',
+        },
+        {
+            id: 3,
+            title: 'Linksmos',
+        },
+        {
+            id: 3,
+            title: 'Linksmos1',
+        },
+        {
+            id: 3,
+            title: 'Linksmos2',
+        },
+        {
+            id: 3,
+            title: 'Linksmos3',
+        },
+        {
+            id: 3,
+            title: 'Linksmos4',
+        },
+        {
+            id: 3,
+            title: 'Linksmos5',
+        },
+        {
+            id: 3,
+            title: 'Linksmos',
+        },
     ]
 
     private _stories: Story[] = [
