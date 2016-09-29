3ed5cb7665fd9e992f92d4b4f55d977698937a24
Paulius R <paulius.repsys@visma.com>
2016-09-29 20:34:04 +0300

    Use bottom bar in all pages

--- app/components/bottom-bar/bottom-bar.component.ts
+++ app/components/bottom-bar/bottom-bar.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 import { Router } from '@angular/router';
 
 @Component({
-    selector: "pas-bottom-bar",
+    selector: "pas-layout-with-bottom-bar",
     templateUrl: "components/bottom-bar/bottom-bar.template.html",
 })
 export class BottomBarComponent {
