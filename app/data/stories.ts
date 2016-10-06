5208b29095b4e8d78e19a71016d8bf8324fe67cc
Paulius R <paulius.repsys@visma.com>
2016-10-06 21:45:06 +0300

    Added stories of category "Apie gamtą"

--- app/data/stories.ts
+++ app/data/stories.ts
@@ -62,12 +62,16 @@ export const stories = [
         length: 235,
         time: 1,
         text: `
-            Kažkada labai toli, puikiuose rūmuose, gyveno turtingas žmogus, vardu Mėlynbarzdis.
-
-            Tokią keistą pavardę jis gavo todėl, kad jo barzda buvo mėlyna. Pažiūrėti jis buvo rūstus ir žiaurus, o mėlyna barzda darė jį tokį baisų, kad niekas nenorėjo už jo tekėti. O Mėlynbarzdis jau šešis sykius buvo vedęs, tiktai niekas nežinojo, kur jo pačios dingsta.
-
-            Tuo metu, kai prasideda mūsų pasaka, Mėlynbarzdis atvažiavo pas savo kaimynę našlę prašyti, kad leistų už jo vieną savo dukrelę — tą, kuri pati panorės už jo eiti. Kai sužinojo gražuolės sesers tas piršlybas, ėmė savitarpy bartis ir siuntinėti jaunikį viena pas kitą. Mėlynbarzdis po to iš_kėlė puotą, susikvietė į ją kaimynus su dukterimis ir visus nustebino savo turtu: šilkai, deimantai ir šydai visur šlamėjo, krištolinės žvakidės visur skleidė šviesos spindulius, didžiausi veidrodžiai rodė visą žmogų, paveikslai ir marmurinės stovylos puošė sienas.
-                `,
+            <p>Seniai seniai, kai dar žmonių nebuvo, gyveno mėnulis ir saulė. gyveno šviesioje pilyje ir vienas kitą labai mylėjo. Begyvendami susilaukė ir gražios dukters. Dukteriai davė Žemės vardą.</p>
+            <p>Daug metų Mėnulis su Saule gražiai gyveno, bet vieną dieną ėmė ir susipyko.</p>
+            <p>- Tu tokia karštuolė, kad aš negaliu su tavim gyventi, - sako Mėnulis. - Jeigu nekeisi savo būdo, aš tave pamesiu.</p>
+            <p>- O tu šaltas kaip ledas. Jei toliau toks būsi - mudu negalėsime gyventi.</p>
+            <p>- Galim skirtis, - ėmė pykti Mėnulis, - bet duktė liks pas mane.</p>
+            <p>- Ne, dukters aš tau nepaliksiu. Ji negalės gyventi su tokiu šaltuoliu.</p>
+            <p>Ilgai juodu ginčijosi ir nesimatė ginčo galo. Pagaliau nutarė prašyti Perkūną, kad surastų teisingą sprendimą.</p>
+            <p>Šis išklausęs Saulę ir Mėnulį sugriaudėjo: - Abudu kalbat niekus. Bet jeigu taip norit skirtis - skirkitės. Tik jūsų duktė Žemė negali priklausyti nei vienam, todėl bus taip: Saulė saugos judviejų dukterį dieną, Mėnulis - naktį.</p>
+            <p>Suprato abu, kad Perkūno sprendimas teisingas, ir šventai jį vykdo: nuo ryto iki vakaro Saulė žiūri dukterį Žemę, o nuo vakaro iki ryto - Mėnulis ją globoja. Kai Mėnulis kur nors išvyksta, tada Žemę prižiūri jo seserys žvaigždės. </p>
+        `,
     },
     {
         id: 4,
@@ -76,25 +80,188 @@ export const stories = [
         length: 269,
         time: 1,
         text: `
-            Kažkada labai toli, puikiuose rūmuose, gyveno turtingas žmogus, vardu Mėlynbarzdis.
-
-            Tokią keistą pavardę jis gavo todėl, kad jo barzda buvo mėlyna. Pažiūrėti jis buvo rūstus ir žiaurus, o mėlyna barzda darė jį tokį baisų, kad niekas nenorėjo už jo tekėti. O Mėlynbarzdis jau šešis sykius buvo vedęs, tiktai niekas nežinojo, kur jo pačios dingsta.
-
-            Tuo metu, kai prasideda mūsų pasaka, Mėlynbarzdis atvažiavo pas savo kaimynę našlę prašyti, kad leistų už jo vieną savo dukrelę — tą, kuri pati panorės už jo eiti. Kai sužinojo gražuolės sesers tas piršlybas, ėmė savitarpy bartis ir siuntinėti jaunikį viena pas kitą. Mėlynbarzdis po to iš_kėlė puotą, susikvietė į ją kaimynus su dukterimis ir visus nustebino savo turtu: šilkai, deimantai ir šydai visur šlamėjo, krištolinės žvakidės visur skleidė šviesos spindulius, didžiausi veidrodžiai rodė visą žmogų, paveikslai ir marmurinės stovylos puošė sienas.
-                `,
+            <p>Žydi sodo medžiai, pavasario šventę švenčia. Oras smagiai kvepia, skaistūs žiedai akį veria.</p>
+            <p>Bitelės, pavasario saulės pažadintos, po orą šaudo; nuo žiedelio į žiedelį nutūpusios, rūpestingai medutį renka.</p>
+            <p>Džiaugiasi vyšnios, obelys, slyvos, kad jas bitelės lanko. Medutį berinkdamos, ant savo kūnelio žiedų dulkeles iš vienos į kitą nešioja. Vieno žiedo dulkelės, į kitą kritusios, gardų vaisių mezga.</p>
+            <p>— Prašom, prašom,— į svečius kviečiasi. — Saldžių sulčių jums žiedeliuose patiekėme. Gerkit sveikos, kiek širdelė geidžia.</p>
+            <p>Atgijo ir kiti sodo gyvūnėliai. Štai šliaužia obels kamienu, prie lapų gretinasi kirmėlaitė. Obelėlė žino, kokia besotė ta viešnia. Apgrauš lapus, šakeles nunuogins. Dar paskui, į lapo likučius Įsi— vyniojusi, voratinkliu apsiraizgys, kybos, kol, drugeliu virtus, išlėks.</p>
+            <p>— Kad žvirblis ar kitas paukštelis greičiau atlėktų, nemalonią viešnią nusineštų,— dūsauja obelėlė.</p>
+            <p>Ir paukšteliai laksto, šakose nardo, medžius nuo kirmėlių valo, tik visų nurinkti nesuspėja.</p>
+            <p>— Lankykitės, paukšteliai,— šnabžda jiems medžiai, šakelėmis linguodami.— Kuriam patinka, mūsų šakelėse lizdelius kraukitės, vaikelius perėkite, savo dainelėmis juos ir mus linksminkite.</p>
+            Ir čiulba, ulba, kvepia sodas per linksmą medžių pavasario šventę.</p>
+        `,
     },
     {
-        id: 4,
+        id: 5,
         categoryId: 1,
         title: 'Žiema',
         length: 213,
         time: 1,
         text: `
-            Kažkada labai toli, puikiuose rūmuose, gyveno turtingas žmogus, vardu Mėlynbarzdis.
-
-            Tokią keistą pavardę jis gavo todėl, kad jo barzda buvo mėlyna. Pažiūrėti jis buvo rūstus ir žiaurus, o mėlyna barzda darė jį tokį baisų, kad niekas nenorėjo už jo tekėti. O Mėlynbarzdis jau šešis sykius buvo vedęs, tiktai niekas nežinojo, kur jo pačios dingsta.
-
-            Tuo metu, kai prasideda mūsų pasaka, Mėlynbarzdis atvažiavo pas savo kaimynę našlę prašyti, kad leistų už jo vieną savo dukrelę — tą, kuri pati panorės už jo eiti. Kai sužinojo gražuolės sesers tas piršlybas, ėmė savitarpy bartis ir siuntinėti jaunikį viena pas kitą. Mėlynbarzdis po to iš_kėlė puotą, susikvietė į ją kaimynus su dukterimis ir visus nustebino savo turtu: šilkai, deimantai ir šydai visur šlamėjo, krištolinės žvakidės visur skleidė šviesos spindulius, didžiausi veidrodžiai rodė visą žmogų, paveikslai ir marmurinės stovylos puošė sienas.
-                `,
+            <p>Atėjo žiema, apsidairė, ar visi jos laukia: ar medžiai ir žolės vasaros apdarus jau nusimetę, sėklas žemėje pasėję, apklostę. Apsidairiusi žemę sustingdė, ant upių ir ežerų ledo tiltus nutiesė, viską baltais sniego patalais apklojo.</p>
+            <p>Stūkso miškai ir sodai sustingę, tūno kietoje žemėje žolės, visi žiemos miegą miega. Miega ir mini, kaip vasarą gražiai gyveno, kaip jauni augo, seni seno, kaip visiems gausiuose saulės spinduliuose buvo smagu klestėti. Kaip medžiuose ir žolėse paukšteliai slapstėsi, nardė, vaikus perėjo, savo giesmeles giedojo, čiulbėjo.</p>
+            <p>Visur tylu. Tik retkarčiais, šiauriui per v daug siausti pradėjus, miškas atsidūsta, sumurma, kad ramiai miegoti neduoda.</p>
+            <p>Miega žiemos miegą miškas, sodas, pievos; miega ir pavasarį sapnuoja. Sapnuoja saulutę, aukštyn j dangų kylančią, iš miego visus juos žadinančią.</p>
+            <p>Ateis pavasaris, atbus, kam per žiemą miegoti lemta; suoš miškas, sušlamės sodas, sužaliuos, margai pasipuoš pievos, laukai; sučiulbės sugrįžę iš šiltesnių kraštų paukšteliai. </p>
+        `,
+    },    
+    {
+        id: 6,
+        categoryId: 2,
+        title: 'Dangus griūva',
+        length: 342,
+        time: 2,
+        text: `
+            <p>Vieną sykį katinas išėjo iš namų pasipeliaut. landžiojo landžiojo po kopūstų daržą, tiktai piept jam ant uodegos galo kopūsto lapas ir užkrito. Persigandęs katinas pamanė: „Dabar tai jau tikrai dangus griūva!" Ir pasileido bėgt.</p>
+            <p>Bėga, spiria, kiek tik kojos leidžia, ir susitinka kiškį.</p>
+            <p>- Kiški tiški, bėkim - dangus griūva!</p>
+            <p>- Kas tau sakė?</p>
+            <p>- Man ant uodegos galo užgriuvo!</p>
+            <p>Kiškis persigandęs bėga drauge. Susitinka lapę. Kiškis šaukia:</p>
+            <p>- Lape snape, bėkim - dangus griūva!</p>
+            <p>- Kas tau sakė?</p>
+            <p>- Katins patins.</p>
+            <p>- Katin patin, kas tau sakė?</p>
+            <p>- Man pačiam ant uodegos galo užgriuvo!</p>
+            <p>Ir lapė persigandusi bėga drauge. Susitinka vilką. Lapė šaukia:</p>
+            <p>- Vilke pilke, bėkim - dangus griūva!</p>
+            <p>- Kas tau sakė?</p>
+            <p>- Kiškis tiškis.</p>
+            <p>- Kiški tiški, kas tau sakė?</p>
+            <p>- Katins patins.</p>
+            <p>- Katin patin, kas tau sakė?</p>
+            <p>- Man pačiam ant uodegos galo užgriuvo!</p>
+            <p>Ir vilkas persigandęs bėga drauge. Susitinka mešką. Vilkas šaukia:</p>
+            <p>- Meška pleška, bėkim - dangus griūva!</p>
+            <p>- Kas tau sakė? Lapė snape.</p>
+            <p>- Lape snape, kas tau sakė?</p>
+            <p>- Kiškis tiškis.</p>
+            <p>- Kiški tiški, kas tau sakė?</p>
+            <p>- Katins patins.</p>
+            <p>- Katin patin, kas tau sakė?</p>
+            <p>- Man pačiam ant uodegos galo užgriuvo!</p>
+            <p>- Papasakok, kaip buvo? - klausia meška.</p>
+            <p>- Ogi man per kopūstų lysvę beeinant kažin kas ūžt ant uodegos galo ir užgriuvo!</p>
+            <p>- Ar didelis?</p>
+            <p>- Kaip gaidžio sparnas.</p>
+            <p>- Ei tu, paikuti, - sako meška, - juk tai tiktai buvo kopūsto lapas. Grįžkite visi namo.</p>
+            <p>- O gal ir teisybė, - tarė katinas. - Tai dabar be baimės galėsime sugrįžti.</p>
+            <p>Ir parbėgo visi namo.</p>
+        `,
+    },
+    {
+        id: 7,
+        categoryId: 2,
+        title: 'Du ožiukai ant liepto.',
+        length: 201,
+        time: 1,
+        text: `
+            <p>Kaimo gale, prie upelio, gyveno ožka su savo vaikeliu - juodu ožiuku. O už upelio, prie tankaus eglyno, trobelėje gyveno kita ožka su baltu ožiuku.</p>
+            <p>Per upelį buvo nutiestas siauras siauras lieptas, per kurį visi eidavo atsargiai, kad neįpultų į vandenį.</p>
+            <p>Abu ožiukai dažnai žvilgčiodavo vienas į kitą. Baltasis norėdavo bėgti į kaimą, o juodasis - į eglyną. Bet ir vienas, ir kitas bijojo eiti per lieptą.</p>
+            <p>Vieną gražią dieną, kai saulutė taip skaisčiai švietė ir visur žydėjo gėlelės, abu ožiukai lyg susitarę patraukė prie upelio.</p>
+            <p>Neužleisdami vienas kitam, juodu skubėjo pereiti lieptu.</p>
+            <p>Ir abu susidūrė liepto viduryje.</p>
+            <p>- Me-e-e! - subliovė baltasis ožiukas. - Pasitrauk iš kelio. Aš noriu pirmas pereiti lieptu.</p>
+            <p>- Dar ko! - supyko juodasis. - Traukis pats, nes bus blogai.</p>
+            <p>Abu ožiukai nenorėjo pasitraukti iš kelio ir įsibėgėję smogė vienas kitam ragais.</p>
+            <p>Nuo smūgio abu tik keberiokšt ir įkrito į upelį. - Gelbėkit! - ėmė šaukti peštukai. Laimė, netoli buvo ožiukų mamos ir atbėgusios ištraukė iš vandens užsispyrėlius.</p>
+        `,
+    },
+    {
+        id: 8,
+        categoryId: 2,
+        title: 'Varna ir sūris',
+        length: 237,
+        time: 1,
+        text: `
+            <p>Šeimininkė suspaudė gražų didelį sūrį ir padėjo jį džiūti ant palangės.</p>
+            <p>Kur buvus, kur nebuvus, varna tik plast plast ir prišokus jį pagriebė.</p>
+            <p>Pagriebus atsitūpė medžio viršūnėje ir patenkinta apsidairė.</p>
+            <p>Tuo metu iš miško atslinko lapė ir pamatė varną su sūriu.</p>
+            <p>Lapei iš alkio ištįso seilė ir tuoj ėmė galvoti, kaip jį išvilioti iš varnos.</p>
+            <p>Bet varna tupi medžio viršūnėje, ir jos nepasieksi.</p>
+            <p>- Laba diena, kaimynėle, - ėmė kalbinti lapė varną. - Kaip gražiai žvilga tavo plunksnos, kokia daili galva, kaip tu moki aukštai skraidyti!</p>
+            <p>Varna iš pasitenkinimo net aukščiau pakėlė uodegą ir norėjo padėkoti lapei, bet pabijojo išmesti sūrį.</p>
+            <p>- O tavo balsas tai gražiausias iš visų paukščių. Kai giedi - tikra paukščių karalienė. Nagi pagiedok ką nors, - meilikavo lapė.</p>
+            <p>Neatsilaikė varna lapės žodžiams ir suriko: „Kar!"</p>
+            <p>Sūris lept iš snapo ir įkrito lapei į nasrus.</p>
+            <p>Nusijuokė lapė ir nuskuodė į mišką pusryčiauti: jai taip kvepėjo šviežias skanus sūris.</p>
+            <p>O varna, tupėdama medžio viršūnėje, ėmė verkti, kad dėl savo kvailumo patikėjo lapės žodžiais ir neteko sūrio.</p>
+        `,
+    },
+    {
+        id: 9,
+        categoryId: 2,
+        title: 'Vilkas ir septyni ožiukai',
+        length: 264,
+        time: 1,
+        text: `
+            <p>Pamiškėje trobelėje gyveno ožka su septyniais ožiukais.</p>
+            <p>Kartą eidama į mišką ožka sušaukė ožiukus ir sako:</p>
+            <p>- Aš einu parnešti šieno, o jūs neįleiskite trobon vilko. Jį pažinsite iš storo balso.</p>
+            <p>- Gerai, motute, - sušuko ožiukai. - Mes jau mokėsim pasisaugoti!</p>
+            <p>Vilkas girdėjo ožkos kalbą, o jai išėjus tuojau atbėgo prie trobelės ir sušuko:</p>
+            <p>- Vaikai, atidarykit duris!</p>
+            <p>Bet ožiukai, išgirdę vilko balsą, atsakė:</p>
+            <p>- Oi, koks storas balsas. Čia ne mūsų motutė.</p>
+            <p>Vilkas nubėgo pas kalvį ir paprašė paploninti liežuvį. Kalvis liepė vilkui padėti liežuvį ant priekalo ir jį paplonino.</p>
+            <p>Dabar vilkas atbėgo prie trobelės ir šaukia:</p>
+            <p>- Įleiskit, vaikeliai! Čia jūsų motutė.</p>
+            <p>Ožiukai, išgirdę ploną balsą, manė, kad grįžo motina, ir įleido vilką į trobą.</p>
+            <p>Vilkas puolė ir prarijo visus ožiukus.</p>
+            <p>Netrukus grįžo ožka. Rado atdaras duris, o trobos viduje gulėjo vilkas ir kietai miegojo.</p>
+            <p>Ožka, pamačiusi, kad vilko pilve kruta ožiukai, paėmusi žirkles prakirpo vilko pilvą ir visi ožiukai iššoko laukan.</p>
+            <p>Tada ožka pridėjo vilkui į pilvą akmenų ir vėl gražiai viską užsiuvo.</p>
+            <p>Pabudęs vilkas panoro gerti ir nuėjo prie upės. Kai tik pasilenkė prie vandens, akmenys jį nutraukė į gilumą ir vilkas prigėrė.</p>
+            <p>O ožka su ožiukais labai džiaugėsi.</p>
+        `,
+    },
+    {
+        id: 10,
+        categoryId: 2,
+        title: 'Žmogus, meška ir lapė',
+        length: 615,
+        time: 3,
+        text: `
+            <p>Vieną kartą arė žmogus dirvą pagiry. Netoliese gulėjo meška lauže. Pamačiusi žmogų, ji ir šaukia:</p>
+            <p>— Žmogau, žmogau, aš tavo jautelius pjausiu.</p>
+            <p>Nusiminė žmogus, nežino, kaip nuo meškos gelbėtis, o čia pasisuko lapė ir sako:</p>
+            <p>— Ką man duosi, jeigu aš tavo jautelius išgelbėsiu?</p>
+            <p>— Aš tau atnešiu vištų maišą, tik, susimildama, neduok meškai mano jautelių papjauti.</p>
+            <p>Taip juodu susitarė, o lapė girion smuko. Atbėgusi iš kito krašto, didžiu balsu šaukė:</p>
+            <p>— Žmogau, žmogau, ar nematei kur netoli meškų, stirnų, vilkų, šernų? Ponas medžioklėn išjojo su penkiais šimtais medžiotojų, o šunų skalikų kita tiek.</p>
+            <p>— Nemačiau nė meškų, nė stirnų, nė vilkų, nė šernų,— tarė žmogus.</p>
+            <p>— O kas ten lauže guli?</p>
+            <p>— Tai šiekšta guli.</p>
+            <p>— Kad būt šiekšta, tai būt šakos apgenėtos,— sako lapė ir vėl nubėgo j gi-</p>
+            <p>Meška išsigando pono ir skalikų ir sako:</p>
+            <p>— Žmogau, nukapok man kojas.</p>
+            <p>Žmogus nukapojo, o lapė vėl išbėgusi iš girios šaukia:</p>
+            <p>— Žmogau, žmogau, ar nematei meškų, stirnų, vilkų? Ponas medžioklėn išjojo. Medžiotojų penki šimtai, o šunų — kita tiek.</p>
+            <p>— Nemačiau,— atsakė žmogus.</p>
+            <p>— O kas ten lauže guli?</p>
+            <p>— Tai medis guli.</p>
+            <p>— Kad būt medis, būtų galan kirvis įkirstas.</p>
+            <p>Ir vėl lapė nubėgo į girią, o meška išsigandusi šaukia:</p>
+            <p>— Žmogau, įkirsk man kirvį galvon.</p>
+            <p>Kai įkirto, tai meška ir galą gavo. Tada atbėgo lapė pas tą žmogų ir kaulija: *</p>
+            <p>— Matai, kaip aš tavo jautelius išgelbėjau, tai tu rytoj atnešk man maišą vištų, kaip esi žadėjęs.</p>
+            <p>Kitą dieną žmogus pasišaukė savo šunis, sukišo juos į maišą, užrišo ir nunešė. Rado lapę belaukiančią, iš to būsimo skanumo besilaižančią.</p>
+            <p>— Paleisk, žmogau, višteles, aš jas pati pasigausiu.</p>
+            <p>Žmogus iškratė iš maišo šunis, o tie skalydami ėmė lapę vytis. Lapė puolė strimagalviais savo olon, kvapo neatgaudama, o šunys, lauke likę, lojo iš piktumo, vos iš kailio neišsinerdami. O lapė stebisi, kailį sveika išnešusi, ir savo akelių klausinėja.</p>
+            <p>— Akelės, akelės, ką jūs darėt, man bebėgant?</p>
+            <p>— Mes tik dairėmės, ar toli dar ola, ar geru taku bėgame.</p>
+            <p>— Gerai darėt, akelės. O jūs, mano auselės, ką beveikėte?</p>
+            <p>— Mes stačios klausėmės, ar toli skalikai loja.</p>
+            <p>— O tu, nosele?</p>
+            <p>— Aš vis uostinėjau, kad kokia bėda už akių arba iš šalies neužbėgtų.</p>
+            <p>— O jūs, kojelės?</p>
+            <p>— Skubinomės viena paskui kitą greičiau susimesti, tarsi lenktynių bėgdamos.</p>
+            <p>— Gerai darėt. O tu, uodegėle, ką veikei, paskui besivilkdama?</p>
+            <p>— Makalavau, tabalavau, kad tik ne mane, o tave šunys pagriebtų.</p>
+            <p>— Ak tu šitokia! — sušuko lapė.— Tekit, šunys, bjaurybę uodegą! — ir iškišo uodegą iš olos. šunys, uodegą nutvėrę, lapę iš olos išvilko ir užkrimto.</p>
+            <p>Kvailas kaip nukirptas avinas. Svetimo netykok, savo nepamesk.</p>
+        `,
     },
 ];
