cbd87c050c09192d6516af1de1166645312b7df1
Paulius R <paulius.repsys@visma.com>
2016-10-06 19:49:56 +0300

    Added several stories

---
+++ app/data/songs.ts
@@ -0,0 +1,62 @@
+export const songs = [
+    {
+        id: 1,
+        title: 'Aleliuja',
+        fileName: 'aleliuja.mp3',
+    },
+    {
+        id: 2,
+        title: 'Angelėliui',
+        fileName: 'angeleliui.mp3',
+    },
+    {
+        id: 3,
+        title: 'Baltoji varnele',
+        fileName: 'baltoji-varnele.mp3',
+    },
+    {
+        id: 4,
+        title: 'Lašeliai',
+        fileName: 'laseliai.mp3',
+    },
+    {
+        id: 5,
+        title: 'Mamytės akys',
+        fileName: 'mamytes-akys.mp3',
+    },
+    {
+        id: 6,
+        title: 'Motule',
+        fileName: 'motule.mp3',
+    },
+    {
+        id: 7,
+        title: 'Naktis',
+        fileName: 'naktis.mp3',
+    },
+    {
+        id: 8,
+        title: 'Šią vasarėlę',
+        fileName: 'sia-vasarele.mp3',
+    },
+    {
+        id: 9,
+        title: 'Svirplys',
+        fileName: 'svirplys.mp3',
+    },
+    {
+        id: 10,
+        title: 'Tavo delne',
+        fileName: 'tavo-delne.mp3',
+    },
+    {
+        id: 11,
+        title: 'Tuk tuk, širdele',
+        fileName: 'tuk-tuk-sirdele.mp3',
+    },
+    {
+        id: 11,
+        title: 'Tyli naktis',
+        fileName: 'tyli-naktis.mp3',
+    },
+];
--- app/data/stories.ts
+++ app/data/stories.ts
@@ -6,12 +6,24 @@ export const stories = [
         length: 275,
         time: 2,
         text: `
-            Kažkada labai toli, puikiuose rūmuose, gyveno turtingas žmogus, vardu Mėlynbarzdis.
-
-            Tokią keistą pavardę jis gavo todėl, kad jo barzda buvo mėlyna. Pažiūrėti jis buvo rūstus ir žiaurus, o mėlyna barzda darė jį tokį baisų, kad niekas nenorėjo už jo tekėti. O Mėlynbarzdis jau šešis sykius buvo vedęs, tiktai niekas nežinojo, kur jo pačios dingsta.
-
-            Tuo metu, kai prasideda mūsų pasaka, Mėlynbarzdis atvažiavo pas savo kaimynę našlę prašyti, kad leistų už jo vieną savo dukrelę — tą, kuri pati panorės už jo eiti. Kai sužinojo gražuolės sesers tas piršlybas, ėmė savitarpy bartis ir siuntinėti jaunikį viena pas kitą. Mėlynbarzdis po to iš_kėlė puotą, susikvietė į ją kaimynus su dukterimis ir visus nustebino savo turtu: šilkai, deimantai ir šydai visur šlamėjo, krištolinės žvakidės visur skleidė šviesos spindulius, didžiausi veidrodžiai rodė visą žmogų, paveikslai ir marmurinės stovylos puošė sienas.
-                `,
+            <p>Augo sode graži obelis. Ėmė obelėlė ir susirgo. Lapai paretėjo, viena šaka visai nudžiūvo. Kartą atsitūpė ant sausosios šakos varna snapo nusivalyti.</p>
+            <p>— Varnele varnele, ar negalėtum manęs pagydyti? — sudejavo obelėlė.</p>
+            <p>Varna apsidairė, apsidairė ir sako:</p>
+            <p>— Maža man iš tavęs nauda, bet tiek to: liepsiu nuo tavęs kirmėles nuvalyti.</p>
+            <p>Nulėkė į lauką, surado varnėnų pulką ir atvarė į obelį.</p>
+            <p>Varnėnai sušokę kirmėles nulesė.</p>
+            <p>Obeliai palengvėjo, lapai kiek sutirštėjo, bet visiškai nepagijo.</p>
+            <p>Varnai vėl teko nutūpti ant sausosios šakos snapo nusivalyti.</p>
+            <p>— Varnele varnele! Dar man vis negera,— pasiskundė obelis.</p>
+            <p>Varna apžiūrėjo obels kamieną, galvą pakraipė ir tarė:</p>
+            <p>— Čia, rodos, nieko nepadarysi be daktaro.</p>
+            <p>Pakilusi nulėkė į mišką. Atsitūpusi į medį, paklausė, paklausė, kur stuksena, ir nulėkė prie senos pušies. Rado genį, pušies kamieną bekalenantį. Pakarksėjo geniui į ausį ir nulėkė savais keliais.</p>
+            <p>Genys, metęs pušį, nulėkė obels apžiūrėti. Pastuksėjo snapu į kamieną ir tarė:</p>
+            <p>— Čionai užteks darbo kelioms dienoms.</p>
+            <p>Apsižiūrėjo vietą nakvynei ir stojo prie darbo. Tris dienas išsijuosęs kaleno, nuo vieno šono gerą plotą žievės nudraskė, vabalus išgaudė.</p>
+            <p>— Dabar pasveiksi,— tarė, darbą pabaigęs.</p>
+            <p>Obelis pasveiko.</p>
+        `,
     },
     {
         id: 2,
@@ -20,12 +32,28 @@ export const stories = [
         length: 481,
         time: 3,
         text: `
-            Kažkada labai toli, puikiuose rūmuose, gyveno turtingas žmogus, vardu Mėlynbarzdis.
-
-            Tokią keistą pavardę jis gavo todėl, kad jo barzda buvo mėlyna. Pažiūrėti jis buvo rūstus ir žiaurus, o mėlyna barzda darė jį tokį baisų, kad niekas nenorėjo už jo tekėti. O Mėlynbarzdis jau šešis sykius buvo vedęs, tiktai niekas nežinojo, kur jo pačios dingsta.
-
-            Tuo metu, kai prasideda mūsų pasaka, Mėlynbarzdis atvažiavo pas savo kaimynę našlę prašyti, kad leistų už jo vieną savo dukrelę — tą, kuri pati panorės už jo eiti. Kai sužinojo gražuolės sesers tas piršlybas, ėmė savitarpy bartis ir siuntinėti jaunikį viena pas kitą. Mėlynbarzdis po to iš_kėlė puotą, susikvietė į ją kaimynus su dukterimis ir visus nustebino savo turtu: šilkai, deimantai ir šydai visur šlamėjo, krištolinės žvakidės visur skleidė šviesos spindulius, didžiausi veidrodžiai rodė visą žmogų, paveikslai ir marmurinės stovylos puošė sienas.
-                `,
+            <p>Viktukas su Antanuku sumanė pažiūrėti, kaip pempės gyvena.</p>
+            <p>Už karklyno buvo plati pelkė. Tenai kas pavasaris parlėkdavo pempių.</p>
+            <p>Vaikai, nubėgę į karklyną, palengva nuslinko tarp karklų, kad pempės nepajustų.</p>
+            <p>Išėję rėplomis į kraštą, pritūpę žiūri, kad kelios pempės nardyte nardo ore: iškyla aukštyn, ore persiverčia ir krinta žemyn; beveik pasiekusios žemę, metasi j vieną šoną, paskui į kitą ir vėl kyla aukštyn, o kitos, galvas iškėlusios, į jas žiūri.</p>
+            <p>— Tai jos pavasario šventę švenčia,— paaiškino Viktukas,— aš ne pirmą kartą tai matau.</p>
+            <p>— Eime tolyn, aš iš arčiau noriu pamatyti,— pasiūlė Antanukas ir atsistojo.</p>
+            <p>Tuojau kelios pempės sušuko: „Gyvi, gyvi!" — ir pasileido lėkti prie vaikų.</p>
+            <p>— Jau mudu pamatė, — tarė Viktukas,— žiūrėk: žvalgai atlekia.</p>
+            <p>Atlėkusios pempės skrajojo aplink vaikus, kone kepures savo sparnais užgaudamos. Juo tolyn ėjo juodu j pelkę, juo daugiau palydovų skrajojo aplinkui, juo neramiau jie šaukė: „Gyvi, gyvi! Neikit į pelkę, lizdų nedraskykitl"</p>
+            <p>— Antanuk, nesumindyk lizdo! — staiga sušuko Viktukas, nutvėręs draugą už rankovės.</p>
+            <p>Ant kupstelio, duobutėje, žole išklotoje, gulėjo prie kits kito neturi. Kiaušiniai, tamsiai rudi, su juodais brūkšneliais ir taškeliais. Vos tebuvo matyti.</p>
+            <p>Antanukas norėjo vieną kiaušinį namo parsinešti. Viktukas sudraudė.</p>
+            <p>— Ką veiksi parsinešęs? Pempėms bus nuoskauda. Geriau būkim paukščių bičiuliai ir kitus drauskim, kad paukščių neskaustų.</p>
+            <p>— Tai dar kada ateisim,— paprašė Antanukas,— aš noriu mažų pempiukų pamatyti.</p>
+            <p>Vaikai įsižiūrėjo lizdo vietą ir susitarė po keliolikos dienų vėl ateisią.</p>
+            <p>Kaip tarė, taip ir padarė. Kelioliktą dieną eina į pelkę. Pempės dar smarkiau vaitoja, pro pat veidus sparnais švilpina.</p>
+            <p>Lizdo ilgai negali rasti. Atitinka pagaliau kupstelį, ant jo duobelę. Tik kiaušinių vietoje keli tamsiai rudi grumsteliai guli. Kurgi kiaušiniai, kur pempiukai? Pasilenkę pažiūrės, kas čia padaryta, kas po tais grumstais yra. O duobelėje ne grumstai, bet keturi pempiukai kaip įsprausti guli, ir nė krust.</p>
+            <p>Pempė:</p>
+            <p>Sveiki gyvi, Sveiki gyvi, Sveiki gyvi, vaikučiai!</p>
+            <p>Klyvis, klyvis, Kas mane padyvys? Kiaušinėlius dėsiu, Vaikelius perėsiu. Kiaušinėliai taškuoti, Vaikeliai kuoduoti.</p>
+            <p>Lietuvių liaudies daina: Pempė bėgo upeliu, nešė šieną glėbeliu. Oi dū, oi dū dū ir pempiukai abudu.</p>
+        `,
     },
     {
         id: 3,
